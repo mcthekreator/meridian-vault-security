7365ec69d9ad801b1bd5ee019a82027c81fecc32
mcthekreator <mcadamsrd@gmail.com>
2025-02-07 20:48:07 +0000

    chore: components setup

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { SubHeadingComponent } from '../sub-heading/sub-heading.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
     HeaderComponent,
+    SubHeadingComponent,
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
