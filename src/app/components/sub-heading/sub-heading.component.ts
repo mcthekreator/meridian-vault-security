ac72aa4240fe0773bfb3c9d9ecc019382528b527
mcthekreator <mcadamsrd@gmail.com>
2025-02-07 20:45:45 +0000

    chore: adding components and libraries

---
+++ src/app/components/sub-heading/sub-heading.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-heading',
+  standalone: true,
+  imports: [],
+  templateUrl: './sub-heading.component.html',
+  styleUrl: './sub-heading.component.scss'
+})
+export class SubHeadingComponent {
+
+}
---
+++ src/app/lib/our-services-card/our-services-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-services-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './our-services-card.component.html',
+  styleUrl: './our-services-card.component.scss'
+})
+export class OurServicesCardComponent {
+
+}
