2b9f565395f922e0ac72b19eb620ec4236acc9ad
mcthekreator <mcadamsrd@gmail.com>
2025-02-07 20:49:04 +0000

    chore: components setup

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { SubHeadingComponent } from '../sub-heading/sub-heading.component';
+import { OurServicesComponent } from '../our-services/our-services.component';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,6 +9,7 @@ import { SubHeadingComponent } from '../sub-heading/sub-heading.component';
   imports: [
     HeaderComponent,
     SubHeadingComponent,
+    OurServicesComponent,
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
