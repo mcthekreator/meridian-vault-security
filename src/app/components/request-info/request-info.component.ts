ee9e2a159061ed03df01630b7b19b794fa8173d2
mcthekreator <mcadamsrd@gmail.com>
2025-02-08 20:54:53 +0000

    chore: components setup

---
+++ src/app/components/request-info/request-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './request-info.component.html',
+  styleUrl: './request-info.component.scss'
+})
+export class RequestInfoComponent {
+
+}
